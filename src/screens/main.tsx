378ffadf98e002410825d126ef067b204499c26f
hl11jp <nguyenducthien9@gmail.com>
2023-02-02 15:39:07 +0700

    added dark theme toggle

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,1 +1,27 @@
 import React from 'react'
+import {
+  Box,
+  Center,
+  Text,
+  VStack,
+  useColorModeValue
+} from 'native-base'
+import ThemeToggle from '../components/ThemeToggle'
+
+export default function MainScreen() {
+  return (
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
+      px={100}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
+          <Text>Hello</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  )
+}
