05a43340f2eda8f33892117f5c7bb34c4a6d7ce0
iuuukhueeeee <nguyenducthien9@gmail.com>
2023-02-09 14:46:42 +0700

    added task item component

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -3,6 +3,7 @@ import { Box, Center, Text, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../components/ThemeToggle'
 import { Pressable } from 'react-native'
 import AnimatedCheckbox from 'react-native-checkbox-reanimated'
+import TaskItem from '../components/TaskItem'
 
 export default function MainScreen() {
   const [checked, setChecked] = useState(false)
@@ -19,9 +20,7 @@ export default function MainScreen() {
     >
       <VStack space={5} alignItems="center">
         <Box w="100px" h="100px">
-          <Pressable onPress={handlePressCheckbox}>
-            <AnimatedCheckbox checked={checked} highlightColor="#4444ff" checkmarkColor="#ffffff" boxOutlineColor="#000000" />
-          </Pressable>
+          <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} />
         </Box>
         <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
           <Text>Hello</Text>
